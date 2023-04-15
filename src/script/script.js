d69fc261dec452d593fa2e76c574e7303327e0a0
Caroline Vézina <caroline_vezina@hotmail.com>
2023-04-15 14:16:28 +0200

    update up button

--- src/script/script.js
+++ src/script/script.js
@@ -13,32 +13,25 @@ document.querySelectorAll("menu-link").forEach(n => n.addEventListener("click", 
 }));
 
 
+// Get the button:
+let upButton = document.getElementById("upButton");
+// When the user scrolls down 100px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+function scrollFunction() {
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    upButton.style.display = "block";
+  } else {
+    upButton.style.display = "none";
+  }
+}
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
 
 //disable save image option
 $(document).on('contextmenu', 'img', function() {
 	return false;
 });
-
-
-
-//comment section
-//var feild = document.querySelector('.container textarea');
-//var backUp = feild.getAttribute('placeholder');
-//var btn = document.querySelector('.container .btn');
-//var clear = document.getElementById('clear');
-//
-//feild.onfocus = function() {
-//  this.setAttribute('placeholder', '');
-//  this.style.borderColor = '#333';
-//  btn.style.display = 'block';
-//}
-//
-//feild.onblur = function() {
-//  this.setAttribute('placeholder', backUp);
-//  this.style.borderColor = '#aaa'
-//}
-//
-//clear.onclick = function() {
-//  btn.style.display = 'none';
-//  feild.value = '';
-//}
